f36dac163c66d8f3abdbb6afd6d7599f121990cd
mose <nekronmoseley@gmail.com>
2024-01-11 22:47:45 +0000

    Forgot return

--- src/main/java/org/mose/command/CommandArgumentResult.java
+++ src/main/java/org/mose/command/CommandArgumentResult.java
@@ -1,6 +1,7 @@
 package org.mose.command;
 
 import org.jetbrains.annotations.NotNull;
+import org.mose.command.context.ArgumentContext;
 import org.mose.command.context.CommandArgumentContext;
 
 /**
@@ -13,39 +14,51 @@ import org.mose.command.context.CommandArgumentContext;
  */
 public record CommandArgumentResult<T>(int position, T value) {
 
-	public int getPosition() {
-		return this.position;
-	}
+    /**
+     * This creates a CommandArgumentResult which processes the argument by 1
+     *
+     * @param argumentContext The argumentContext of the command argument in question
+     * @param value           The value of the argument
+     * @param <T>             The type of the value of the argument
+     * @return The CommandArgumentResult
+     * @deprecated use ArgumentContext edition
+     */
+    @Deprecated(forRemoval = true)
+    public static @NotNull <T> CommandArgumentResult<T> from(CommandArgumentContext<T> argumentContext, T value) {
+        return from(argumentContext, 1, value);
+    }
 
-	public T getValue() {
-		return this.value;
-	}
+    /**
+     * This creates a CommandArgumentResult which processes the argument by the specified amount
+     *
+     * @param argumentContext The argumentContext of the command argument in question
+     * @param length          The amount to move the argument over
+     * @param value           The value of the argument
+     * @param <T>             The type of the value of the argument
+     * @return The CommandArgumentResult
+     * @deprecated use ArgumentContext edition
+     */
+    @Deprecated(forRemoval = true)
+    public static @NotNull <T> CommandArgumentResult<T> from(CommandArgumentContext<T> argumentContext, int length,
+                                                             T value) {
+        return new CommandArgumentResult<>(argumentContext.getFirstArgument() + length, value);
+    }
 
-	/**
-	 * This creates a CommandArgumentResult which processes the argument by 1
-	 *
-	 * @param argumentContext The argumentContext of the command argument in question
-	 * @param value           The value of the argument
-	 * @param <T>             The type of the value of the argument
-	 * @return The CommandArgumentResult
-	 */
-	public static @NotNull <T> CommandArgumentResult<T> from(CommandArgumentContext<T> argumentContext, T value) {
-		return from(argumentContext, 1, value);
-	}
+    public static @NotNull <T> CommandArgumentResult<T> from(ArgumentContext context, T value) {
+        return from(context, 1, value);
+    }
 
-	/**
-	 * This creates a CommandArgumentResult which processes the argument by the specified amount
-	 *
-	 * @param argumentContext The argumentContext of the command argument in question
-	 * @param length          The amount to move the argument over
-	 * @param value           The value of the argument
-	 * @param <T>             The type of the value of the argument
-	 * @return The CommandArgumentResult
-	 */
-	public static @NotNull <T> CommandArgumentResult<T> from(CommandArgumentContext<T> argumentContext, int length,
-			T value) {
-		return new CommandArgumentResult<>(argumentContext.getFirstArgument() + length, value);
-	}
+    public static @NotNull <T> CommandArgumentResult<T> from(ArgumentContext context, int length, T value) {
+        return new CommandArgumentResult<>(context.getArgumentIndex() + length, value);
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
 
 
 }
